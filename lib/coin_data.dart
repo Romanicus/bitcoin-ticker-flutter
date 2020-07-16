1e3ecc961c2e3ae6d64d6a4190e71d76335761c0
Nico Rusam <nico.rusam@gmail.com>
2020-07-16 15:36:56 +0200

    finished coin ticker

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,83 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+//Two api secrets in case of the 100 request limit is reached out.
+const String _apiSecret = 'apiKey1';
+const String _apiSecret2 = 'apiKey2';
+const String _url = 'https://rest.coinapi.io/v1/exchangerate';
+const String _getAllDataFor = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  static dynamic _BTCData = [];
+  static dynamic _ETHData = [];
+  static dynamic _LTCData = [];
+
+  static Future<double> getCoinData(String currency) async {
+    http.Response response = await http.get('$_url/BTC/$currency?apikey=$_apiSecret');
+
+    if (response.statusCode == 401 || response.statusCode == 429) {
+      http.Response response2 = await http.get('$_url/BTC/$currency?apikey=$_apiSecret2');
+      return jsonDecode(response2.body)['rate'];
+    }
+
+    print('Response statuscode: ${response.statusCode}');
+    return jsonDecode(response.body)['rate'];
+  }
+
+  static Future<void> updateExchangeData() async {
+    cryptoList.forEach((element) async {
+      Coins coins = Coins.BTC;
+
+      if (element == 'ETH')
+        coins = Coins.ETH;
+      else if (element == 'LTC') coins = Coins.LTC;
+
+      http.Response response = await http.get('$_getAllDataFor/$element?apikey=$_apiSecret');
+      print('Response code is: ${response.statusCode}');
+      if (response.statusCode == 401 || response.statusCode == 429) {
+        http.Response response2 = await http.get('$_getAllDataFor/$element?apikey=$_apiSecret2');
+        print('Response2 code is: ${response2.statusCode}');
+
+        _mapValueToList(jsonDecode(response2.body), coins);
+        return;
+      }
+      _mapValueToList(jsonDecode(response.body), coins);
+    });
+  }
+
+  static void _mapValueToList(dynamic list, Coins coin) {
+    switch (coin) {
+      case Coins.BTC:
+        _BTCData = list;
+        break;
+      case Coins.ETH:
+        _ETHData = list;
+        break;
+      case Coins.LTC:
+        _LTCData = list;
+        break;
+    }
+  }
+
+  static double getCoinExchangeRateFor(Coins coins, String currency) {
+    dynamic listToHandle;
+    double value;
+
+    if (coins == Coins.ETH)
+      listToHandle = _ETHData;
+    else if (coins == Coins.BTC)
+      listToHandle = _BTCData;
+    else if (coins == Coins.LTC) listToHandle = _LTCData;
+    listToHandle['rates'].forEach((element) {
+      String currencyQuote = element['asset_id_quote'];
+      if (currencyQuote == currency) {
+        value = double.tryParse(element['rate'].toString());
+        return;
+      }
+    });
+
+    return value;
+  }
+}
+
+enum Coins { BTC, ETH, LTC }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,79 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedItem = 'USD';
+
+  @override
+  void initState() {
+    super.initState();
+    updateExchangeDataOnStart(selectedItem);
+    print('initState finished');
+  }
+
+  void updateExchangeDataOnStart(String currency) async {
+    await CoinData.updateExchangeData();
+  }
+
+  Future<void> updateExchangeRate(String currency) async {
+    setState(() {
+      selectedItem = currency;
+    });
+  }
+
+  DropdownButton androidDropdown() {
+    List<DropdownMenuItem<String>> dropDownItemsList = [];
+    currenciesList.forEach(
+      (element) => dropDownItemsList.add(DropdownMenuItem(child: Text(element), value: element)),
+    );
+
+    return DropdownButton(
+      value: selectedItem,
+      items: dropDownItemsList,
+      onChanged: (value) {
+        updateExchangeRate(value);
+      },
+    );
+  }
+
+  CupertinoPicker iOsPicker() {
+    List<Widget> itemsList = [];
+    currenciesList.forEach(
+      (element) => itemsList.add(Text(element)),
+    );
+
+    return CupertinoPicker(
+      itemExtent: 32,
+      onSelectedItemChanged: (selectedIndex) {
+        updateExchangeRate(currenciesList[selectedIndex]);
+      },
+      children: itemsList,
+    );
+  }
+
+  Padding makeCard(String currency, Coins coins) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $currency = ${CoinData.getCoinExchangeRateFor(coins, selectedItem)?.toStringAsFixed(2)} $selectedItem',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +92,20 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              makeCard('BTC', Coins.BTC),
+              makeCard('ETH', Coins.ETH),
+              makeCard('LTC', Coins.LTC),
+            ],
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iOsPicker() : iOsPicker(),
           ),
         ],
       ),
